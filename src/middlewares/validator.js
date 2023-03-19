44e839271a44ba13c20361055f156e15b3903bd3
MuffinPlease <benjamintec3455@gmail.com>
2023-03-19 12:38:56 -0600

    CREATE validator middlewares

---
+++ src/middlewares/validator.js
@@ -0,0 +1,22 @@
+const boom = require("@hapi/boom");
+
+function validatorHandler(schema, property = "body") {
+    const validationOptions = { abortEarly: false, stripUnknown: true };
+
+    return (req, res, next) => {
+        const data = req[property];
+        const { error, value } = schema.validate(data, validationOptions);
+
+        if (error) {
+            const message = error.details.map((e) => e.message).join(", ");
+            next(boom.badRequest(message, { details: error.details }));
+            return;
+        }
+
+        // Replace the original data with the validated object.
+        req[property] = value;
+        next();
+    };
+}
+
+module.exports = { validatorHandler };
