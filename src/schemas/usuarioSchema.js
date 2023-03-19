22f38d090615f79b5a21e0075fe9010fdfa96357
MuffinPlease <benjamintec3455@gmail.com>
2023-03-19 12:59:15 -0600

    UDAPTE usarioSchema typo

--- src/schemas/usuarioSchema.js
+++ src/schemas/usuarioSchema.js
@@ -1,36 +1,36 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const id = Joi.number().integer().positive();
 const name = Joi.string().trim().min(2).max(50);
 const lastName = Joi.string().trim().min(2).max(50);
 const email = Joi.string().trim().email().max(100);
 const phone = Joi.string().trim().min(10).max(20);
-const user = Joi.string().trim().min(2).max(50);
+const username = Joi.string().trim().min(2).max(50);
 const password = Joi.string().trim().min(8).max(50);
-const fotoUrl = Joi.string().trim().uri().max(255);
+const photoUrl = Joi.string().trim().uri().max(255);
 
 const getUsuarioSchema = Joi.object({
-  id: id.required(),
+    id: id.required(),
 });
 
 const createUsuarioSchema = Joi.object({
-  name: name.required(),
-  lastName: lastName.required(),
-  email: email.required(),
-  phone: phone.required(),
-  user: user.required(),
-  password: password.required(),
-  fotoUrl: fotoUrl.required(),
+    name: name.required(),
+    lastName: lastName.required(),
+    email: email.required(),
+    phone: phone.required(),
+    username: username.required(),
+    password: password.required(),
+    photoUrl: photoUrl.required(),
 });
 
 const updateUsuarioSchema = Joi.object({
-  name,
-  lastName,
-  email,
-  phone,
-  user,
-  password,
-  fotoUrl,
+    name,
+    lastName,
+    email,
+    phone,
+    username,
+    password,
+    photoUrl,
 });
 
 module.exports = { getUsuarioSchema, createUsuarioSchema, updateUsuarioSchema };
